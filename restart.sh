01573e51a6c09c8bfbc3f5e2e20d89d6b9a15410
Lihus <25803090195@m.fudan.edu.cn>
2025-11-28 00:22:29 +0800

    fix deploy yml couldn't run restart.sh.

--- restart.sh
+++ restart.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-# 如果是 Node.js + pm2：
-if command -v pm2 >/dev/null 2>&1; then
-  pkill -f "node server.js" || true
-  sleep 2
-  node server.js
-fi
-
+pkill -f "node server.js" || true
+sleep 2
+node server.js
 
 echo "Restart script finished."
